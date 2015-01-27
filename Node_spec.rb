6a5e62295daf9942b852bd1fa4b385865746acca
Denis Laliberté <denis@tp1.ca>
2015-01-26 22:52:58 -0500

    tree element list

--- Node_spec.rb
+++ Node_spec.rb
@@ -7,11 +7,13 @@ describe "list" do
     expect(list.head.value).to eq(1)
     expect(list.end.value).to eq(1)
   end
-  it "add two item in a list" do
+  it "add three item in a list" do
     list = List.new()
     list.append(1)
     list.append(2)
     expect(list.end.value).to eq(2)
     expect(list.head.value).to eq(1)
+    list.append(3)
+    expect(list.end.value).to eq(3)
   end
 end
