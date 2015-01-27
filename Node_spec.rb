44e00120addcc68b5a2095975bff81ead6d1f176
Denis Laliberté <denis@tp1.ca>
2015-01-26 20:45:47 -0500

    add one value

--- Node_spec.rb
+++ Node_spec.rb
@@ -2,7 +2,7 @@ require './Node.rb'
 
 describe "list" do
   it "create a new item" do
-    n = Node.new(1)
-    expect(true).to eq(true)
+    node = Node.new(1)
+    expect(node.value).to eq(1)
   end
 end
