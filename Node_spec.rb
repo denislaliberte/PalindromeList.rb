0357b48a064ce4b1ee6ee74c1e4dc5b3988bb6d4
Denis Laliberté <denis@tp1.ca>
2015-01-26 22:50:58 -0500

    refactor test documentation

--- Node_spec.rb
+++ Node_spec.rb
@@ -1,16 +1,15 @@
 require './Node.rb'
 
 describe "list" do
-  it "create append a value to a list" do
+  it "create a list with one element" do
     list = List.new()
     list.append(1)
     expect(list.head.value).to eq(1)
-  end
-  it "add an item to the end of a list" do
-    list = List.new()
-    list.append(1)
     expect(list.end.value).to eq(1)
-    expect(list.head.value).to eq(1)
+  end
+  it "add two item in a list" do
+    list = List.new()
+    list.append(1)
     list.append(2)
     expect(list.end.value).to eq(2)
     expect(list.head.value).to eq(1)
