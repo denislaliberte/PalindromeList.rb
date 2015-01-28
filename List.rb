b39e6dd297dc91e57956477c358a1b417dccc2b1
Denis Laliberté <denis@tp1.ca>
2015-01-28 12:53:04 -0500

    rextract add to end

--- List.rb
+++ List.rb
@@ -6,21 +6,22 @@ class List
   def append(value)
     newNode = Node.new(value,@end)
     self.add_to_head(newNode)
-
-
-    if @end
-      @end.next = newNode
-      @end = newNode
-    else
-      @end = newNode
-    end
-
+    self.add_to_end(newNode)
     self
   end
 
   def add_to_head(newNode)
     if not @head
      @head = newNode
+    end
+  end
+
+  def add_to_end(newNode)
+    if @end
+      @end.next = newNode
+      @end = newNode
+    else
+      @end = newNode
     end
   end
 
