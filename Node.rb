d9ff844d182a6ba185e6a27537fcc3ce6515260c
Denis Laliberté <denis@tp1.ca>
2015-02-15 22:28:16 -0500

    add next value

--- Node.rb
+++ Node.rb
@@ -1,5 +1,6 @@
 
 class Node
+  attr_accessor :next
   attr_reader :value
 
   def initialize(value)
@@ -14,10 +15,11 @@ class List
   def add(value)
     new = Node.new(value)
     @head = new unless @head
+    @end.next = new unless not @end
     @end = new
     self
   end
   def values
-    [@head.value,@end.value]
+    [@head.value,@head.next.value]
   end
 end
